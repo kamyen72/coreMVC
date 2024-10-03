c070f0300d391100b5c8fc5a79a6193168584a9c
kamyen72 <kamyen.cheah@gmail.com>
2024-10-03 17:57:50 +0800

    Add existing project files to Git

---
+++ coreMVC/Classes/DBs.cs
@@ -0,0 +1,20 @@
+﻿namespace CoreMVC3.Classes
+{
+    public class db_ghl33
+    {
+		public static string connStr = "Server=192.82.60.31;Database=GHL;User Id=GHLUser;Password=@golden85092212;TrustServerCertificate=true;";
+		public static string dbfullname = "GHL";
+		public static string ip = "192.82.60.31";
+		public static string userId = "GHLUser";
+		public static string password = "@golden85092212";
+	}
+
+    public class db_master
+    {
+        public static string connStr = "Server=192.82.60.148;Database=MasterGHL;User Id=MasterUser;Password=@master85092212;TrustServerCertificate=true;";
+        public static string dbfullname = "MasterGHL";
+        public static string ip = "192.82.60.148";
+        public static string userId = "MasterUser";
+        public static string password = "@master85092212";
+    }
+}
